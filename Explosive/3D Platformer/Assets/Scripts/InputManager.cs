c539ad9ab35604144415a309e9f0d83a356d524d
Daniel Badger <144151489+GraniteJedi@users.noreply.github.com>
2025-10-02 21:44:21 -0400

    Re-added Ayele movement

---
+++ Explosive/3D Platformer/Assets/Scripts/InputManager.cs
@@ -0,0 +1,465 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class InputManager : MonoBehaviour
+{
+    private InputAsset moveControls;
+    [SerializeField] PlayerManager playerManager;
+
+    private bool altModeOn = false;
+    private bool isOnWall = false;
+    private Vector3 wallNormal;
+    [SerializeField] int initWallJumps = 3;
+    int wallJumps = 0;
+
+    private bool isGrounded = false;
+    private bool isSlashing = false;
+    private bool isSliding = false;
+    private bool isSlamming = false;
+    private bool isGrappling = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveControls = new InputAsset();
+
+        moveControls.Player.Move.performed += OnMove;
+        moveControls.Player.Move.canceled += OnMove;
+
+        moveControls.Player.Jump.performed += OnJump;
+
+        moveControls.Player.Slide.performed += OnSlide;
+        moveControls.Player.Slide.canceled += OnSlideCancelled;
+
+        moveControls.Player.Grapple.performed += OnGrapple;
+
+        moveControls.Player.Swing.performed += OnSwing;
+
+        moveControls.Player.Look.performed += OnLook;
+
+        moveControls.Player.Slash.performed += OnSlash;
+
+        moveControls.Player.QuickMine.performed += OnQuickMine;
+
+        moveControls.Player.ALTMODE.performed += OnAltMode;
+        //moveControls.Player.ALTMODE.canceled += OnAltMode;    UNCOMMENT THIS TO MAKE ALT ACTIVE WHILE HOLDING RMB
+
+        moveControls.Player.Kamikaze.performed += OnKamikaze;
+
+        moveControls.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// =====================================================================
+    /// ========================== INPUT FUNCTIONS ==========================
+    /// =====================================================================
+    /// 
+    /// All INPUT FUNCTIONS check if an input is valid then pass the appropriate calls to
+    ///     the necessary classes
+
+    void OnMove(InputAction.CallbackContext context)
+    {
+        if (IsMoveValid())
+        {
+            playerManager.Move(context.ReadValue<Vector2>());
+        }
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (IsJumpValid())
+        {
+            Debug.Log($"OnJump | {context.action.name}");
+            playerManager.Jump();
+        }
+        else if (IsWallJumpValid())
+        {
+            Debug.Log($"OnWallJump | {context.action.name}");
+            playerManager.WallJump(wallNormal);
+        }
+    }
+
+    void OnSlide(InputAction.CallbackContext context)
+    {
+        if (IsSlideValid())
+        {
+            Debug.Log($"OnSlide | {context.action.name}");
+            playerManager.Slide();
+        }
+        else if (IsSlamValid())
+        {
+            Debug.Log($"OnSlam | {context.action.name}");
+            playerManager.Slam();
+        }
+    }
+
+    void OnSlideCancelled(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnSlideCancelled | {context.action.name}");
+        if (IsSliding())
+        {
+            playerManager.SlideCancelled();
+        }
+    }
+
+    void OnGrapple(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnMove | {context.action.name}");
+        if (IsGrappling())
+        {
+            playerManager.GrappleCancel();
+        }
+        else if (IsGrappleValid())
+        {
+            playerManager.Grapple();
+        }
+    }
+
+    void OnSwing(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnSwing | {context.action.name}");
+        if (IsSwingValid())
+        {
+            playerManager.Swing();
+        }
+    }
+
+    void OnLook(InputAction.CallbackContext context)
+    {
+        //Debug.Log($"OnLook | { context.action.name }");
+        if (IsLookValid())
+        {
+            playerManager.Look(context.ReadValue<Vector2>());
+        }
+    }
+
+    void OnSlash(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnSlash {context.action.name}");
+        if (IsSlashValid())
+        {
+            playerManager.Slash();
+        }
+    }
+
+    void OnQuickMine(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnQuickMine | {context.action.name}");
+        if (IsQuickMineValid())
+        {
+            playerManager.QuickMine();
+        }
+    }
+
+    void OnAltMode(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnAltMode | {context.action.name}");
+        if (IsAltModeValid())
+        {
+            playerManager.ToggleAltMode();
+        }
+    }
+
+    void OnKamikaze(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnKamikaze | {context.action.name}");
+        if (IsKamikazeValid())
+        {
+            playerManager.Kamikaze();
+        }
+    }
+
+    /// =====================================================================
+    /// ========================== VALIDITY CHECKS ==========================
+    /// =====================================================================
+    /// 
+    /// All VALIDITY CHECK functions check if a certain input action is valid in
+    ///     the current context of the game
+
+    /// <summary>
+    /// Checks if the player is currently in a state to move
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to move and FALSE otherwise
+    /// </returns>
+    private bool IsMoveValid()
+    {
+        return true;
+    }
+
+
+    /// <summary>
+    /// Checks if the player is currently in a state to jump based on if they're on the ground
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to jump and FALSE otherwise
+    /// </returns>
+    private bool IsJumpValid()
+    {
+        return isGrounded;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to wall jump based on if they're colliding with
+    ///     a wall while off of the ground and they still have wall jumps remaining
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to wall jump and FALSE otherwise
+    /// </returns>
+    private bool IsWallJumpValid()
+    {
+        if (IsOnWall() && !IsSlashing() && !IsGrounded() && wallJumps > 0)
+        {
+            wallJumps--;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to slide based on if they're on the ground
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to slide and FALSE otherwise
+    /// </returns>
+    private bool IsSlideValid()
+    {
+        return isGrounded;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to slam based on whether they're airborne
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to slam and FALSE otherwise
+    /// </returns>
+    private bool IsSlamValid()
+    {
+        return !isGrounded;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to grapple (mostly based on cooldown period)
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to grapple and FALSE otherwise
+    /// </returns>
+    private bool IsGrappleValid()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to swing their weapon (mostly based on cooldown period)
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to swing and FALSE otherwise
+    /// </returns>
+    private bool IsSwingValid()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to look around 
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to look around and FALSE otherwise
+    /// </returns>
+    private bool IsLookValid()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to slash (mostly based on cooldown period)
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to slash and FALSE otherwise
+    /// </returns>
+    private bool IsSlashValid()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to throw a quick mine based on cooldowns
+    ///     and if there are any more mines remaining
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to toss a quick mine and FALSE otherwise
+    /// </returns>
+    private bool IsQuickMineValid()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to toggle ALT MODE
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to toggle ALT MODE and FALSE otherwise
+    /// </returns>
+    private bool IsAltModeValid()
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently in a state to sacrifice countdown time for a
+    ///     boost in velocity based on remaining time and cooldown
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is allowed to kamikaze and FALSE otherwise
+    /// </returns>
+    private bool IsKamikazeValid()
+    {
+        return true;
+    }
+
+    /// ==================================================================
+    /// ========================== STATE CHECKS ==========================
+    /// ==================================================================
+    /// 
+    /// All STATE CHECK functions verify whether or not the player is currently in
+    ///     the middle of a certain input action
+
+    /// <summary>
+    /// Checks if the player is currently sliding or not
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is sliding and FALSE otherwise
+    /// </returns>
+    public bool IsSliding()
+    {
+        return isSliding;
+    }
+
+    public void SetSlidingOn()
+    {
+        isSliding = true;
+    }
+
+    public void SetSlidingOff()
+    {
+        isSliding = false;
+    }
+
+    private bool IsOnWall()
+    {
+        return isOnWall;
+    }
+
+    public void SetOnWall(Vector3 wallNormal)
+    {
+        isOnWall = true;
+        this.wallNormal = wallNormal;
+    }
+
+    public void SetOffWall()
+    {
+        isOnWall = false;
+    }
+
+    private bool IsGrounded()
+    {
+        return isGrounded;
+    }
+
+    public void SetOnGround()
+    {
+        wallJumps = initWallJumps;
+        isGrounded = true;
+        playerManager.SetAirResistance(8);
+        if (IsSlamming())
+        {
+            isSlamming = false;
+        }
+    }
+
+    public void SetOffGround()
+    {
+        isGrounded = false;
+        playerManager.SetAirResistance(7);
+        if (IsSliding())
+        {
+            isSliding = false;
+        }
+    }
+
+    public bool IsSlashing()
+    {
+        return isSlashing;
+    }
+
+    public void SetSlashingOn()
+    {
+        isSlashing = true;
+    }
+
+    public void SetSlashingOff()
+    {
+        isSlashing = false;
+    }
+
+    public bool IsSlamming()
+    {
+        return isSlamming;
+    }
+
+    public void SetSlammingOn()
+    {
+        isSlamming = true;
+    }
+
+    public void SetSlammingOff()
+    {
+        isSlamming = false;
+    }
+
+    /// <summary>
+    /// Checks if the player is currently grappling or not
+    /// </summary>
+    /// 
+    /// <returns>
+    /// TRUE if the player is grappling and FALSE otherwise
+    /// </returns>
+    public bool IsGrappling()
+    {
+        return isGrappling;
+    }
+
+    public void SetGrapplingOn()
+    {
+        isGrappling = true;
+    }
+
+    public void SetGrapplingOff()
+    {
+        isGrappling = false;
+    }
+}
--- Explosive/3D Platformer/Assets/Scripts/PlayerManager.cs
+++ Explosive/3D Platformer/Assets/Scripts/PlayerManager.cs
@@ -2,6 +2,425 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+
+    [Header("Camera Settings")]
+    private float lookPitch = 0f;
+    private float lookYaw = 0f;
+    private Vector3 cameraHeight;
+    [SerializeField] float lookSensitivity = 0f;
+    [SerializeField] float FOV = 90f;
+    [SerializeField] Camera playerCamera;
+
+    [Header("Move Settings")]
+    [SerializeField] private Rigidbody playerBody;
+    [SerializeField] private BoxCollider playerCollider;
+    [SerializeField] private float moveSpeed = 12f;
+    [SerializeField] private float speedMultiplyer = 0f;
+    [SerializeField] private float gravBoost = 4f;
+    [SerializeField] private float airResistance;
+    [SerializeField] private int momentumLossDelay = 10;
+    private int delayFrames = 0;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private float wallJumpSpeed;
+
+
+    [Header("Slash Settings")]
+    [SerializeField] private float slashDuration = 0.1f;
+    [SerializeField] private float slashDistance = 5f;
+    [SerializeField] private float slashSpeed = 150.2f;
+
+    [Header("Slam/Slide Settings")]
+    [SerializeField] private float slideTime = 2f;
+    [SerializeField] private float slideSpeed = 15f;
+    [SerializeField] private float slideCameraTime = 0.1f;
+    [SerializeField] private float slideCameraHeight = 0.3f;
+    [SerializeField] private float slideFriction = 0.5f;
+    [SerializeField] private int frictionFrameDelay = 120;
+    [SerializeField] private float slamSpeed = 30f;
+
+    [Header("Speed Multiplyer Settings")]
+    [SerializeField] private float slashMultiplyer = 1.8f;
+    [SerializeField] private float slideMultiplyer = 1.5f;
+    [SerializeField] private float slamMultiplyer = 2f;
+    private Vector3 velocityVector;
+
+    [Header("Grapple Settings")]
+    [SerializeField] private float grappleRange;
+    [SerializeField] private float grappleSpeed;
+    [SerializeField] private float grappleStrength;
+
+    [Header("Physics Materials")]
+    [SerializeField] PhysicMaterial grounded;
+    [SerializeField] PhysicMaterial onWall;
+
+    private Quaternion worldToLocal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraHeight = playerCamera.transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        worldToLocal = Quaternion.FromToRotation(Vector3.forward, playerBody.transform.forward);
+
+        if (GetSpeed() <= moveSpeed && !inputManager.IsSliding())
+        {
+            delayFrames++;
+            if (delayFrames >= momentumLossDelay)
+            {
+                speedMultiplyer = 1f;
+                delayFrames = 0;
+            }
+        }
+        else
+        {
+            delayFrames = 0;
+        }
+
+        if (!inputManager.IsSlashing() && !inputManager.IsSliding() && !inputManager.IsSlamming())
+        {
+            Vector3 maintainVelocity = Vector3.zero;
+
+            if (velocityVector.x == 0)
+            {
+                maintainVelocity += playerBody.transform.right * Vector3.Dot(GetVelocity(), playerBody.transform.right);
+            }
+            if (velocityVector.z == 0)
+            {
+                maintainVelocity += playerBody.transform.forward * Vector3.Dot(GetVelocity(), playerBody.transform.forward);
+            }
+
+            SetVelocity(worldToLocal * (velocityVector * moveSpeed * speedMultiplyer) + maintainVelocity + GetVelocity().y * Vector3.up);
+        }
+
+        playerBody.AddForce(Vector3.down * gravBoost);
+        playerBody.AddForce(Vector3.ProjectOnPlane(-GetVelocity(), playerBody.transform.up) * airResistance);
+    }
+
+    public void Move(Vector2 moveVector)
+    {
+        velocityVector = new Vector3(moveVector.x, 0, moveVector.y);
+    }
+
+    public void Jump()
+    {
+        inputManager.SetSlidingOff();
+        SetVelocity(new Vector3(GetVelocity().x, 0f, GetVelocity().z) + Vector3.up * jumpSpeed);
+    }
+
+    public void WallJump(Vector3 wallNormal)
+    {
+        wallNormal = Vector3.ProjectOnPlane(wallNormal, playerBody.transform.up).normalized;
+
+        SetVelocity(GetVelocity() + wallNormal * wallJumpSpeed);
+
+        SetVelocity(new Vector3(GetVelocity().x, Mathf.Clamp(GetVelocity().y, 0f, 1f), GetVelocity().z) + Vector3.up * jumpSpeed);
+    }
+
+    public void Slide()
+    {
+        StartCoroutine(SlideMove());
+    }
+
+    public void Slam()
+    {
+        StartCoroutine(SlamMove());
+    }
+
+    public void SlideCancelled()
+    {
+        inputManager.SetSlidingOff();
+    }
+
+    public void GrappleCancel()
+    {
+        inputManager.SetGrapplingOff();
+    }
+
+    public void Grapple()
+    {
+        StartCoroutine(GrappleMove());
+    }
+
+    public void Swing()
+    {
+        ;
+    }
+
+    public void Look(Vector2 delta)
+    {
+        lookPitch -= delta.y * lookSensitivity;
+        lookPitch = Mathf.Clamp(lookPitch, -90f, 90f);
+
+        lookYaw = delta.x * lookSensitivity;
+
+        playerCamera.transform.localRotation = Quaternion.Euler(lookPitch, 0f, 0f);
+        playerBody.transform.rotation *= Quaternion.Euler(0f, lookYaw, 0f);
+    }
+
+    public void Slash()
+    {
+        StartCoroutine(SlashMove());
+    }
+
+    public void QuickMine()
+    {
+        ;
+    }
+
+    public void ToggleAltMode()
+    {
+        ;
+    }
+
+    public void Kamikaze()
+    {
+        ;
+    }
+
+    public IEnumerator SlashMove()
+    {
+        inputManager.SetSlammingOff();
+        inputManager.SetSlidingOff();
+        inputManager.SetSlashingOn();
+
+        bool forcedNonzero = false;
+
+        Vector3 initDirection;
+
+        if (velocityVector == Vector3.zero)
+        {
+            forcedNonzero = true;
+            initDirection = playerBody.transform.forward;
+            velocityVector = playerBody.transform.forward;
+        }
+        else
+        {
+            initDirection = Quaternion.FromToRotation(Vector3.forward, playerBody.transform.forward) * velocityVector.normalized;
+        }
+
+        float elapsedTime = 0;
+        speedMultiplyer = slashMultiplyer;
+        delayFrames = 0;
+
+        while (elapsedTime < slashDuration && inputManager.IsSlashing() && !inputManager.IsSliding())
+        {
+            SetVelocity((-2f * Mathf.Lerp(0, 0.5f, elapsedTime / slashDuration) + 2) * slashSpeed * initDirection);
+
+            elapsedTime += Time.fixedDeltaTime;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        if (forcedNonzero)
+        {
+            velocityVector = Vector3.zero;
+        }
+
+        inputManager.SetSlashingOff();
+    }
+
+    public IEnumerator SlideMove()
+    {
+        Vector3 slideDirection = playerBody.transform.forward;
+        Vector3 groundNormal;
+        Vector3 initCameraPos = playerCamera.transform.localPosition;
+        RaycastHit hit;
+        if (speedMultiplyer < slideMultiplyer)
+            speedMultiplyer = slideMultiplyer;
+
+        int frameCount = 0;
+
+        float elapsedTime = 0;
+        inputManager.SetSlidingOn();
+
+        while (inputManager.IsSliding())
+        {
+            LowerCamera(initCameraPos, elapsedTime);
+
+            if (!Physics.Raycast(playerCamera.transform.position, -playerBody.transform.up, out hit, 3.5f, LayerMask.GetMask("Ground")))
+            {
+                break;
+            }
+
+            groundNormal = hit.normal;
+
+            Debug.Log(frameCount);
+            if (frameCount >= frictionFrameDelay)
+            {
+                speedMultiplyer = Mathf.Clamp(speedMultiplyer - slideFriction * Time.fixedDeltaTime, 0, int.MaxValue);
+                if (speedMultiplyer <= 0)
+                    break;
+            }
+
+            SetVelocity(Vector3.ProjectOnPlane(slideDirection * slideSpeed * speedMultiplyer, groundNormal));
+
+            elapsedTime += Time.fixedDeltaTime;
+            frameCount += 1;
+            yield return new WaitForFixedUpdate();
+        }
+
+        inputManager.SetSlidingOff();
+
+        elapsedTime = 0;
+        initCameraPos = playerCamera.transform.localPosition;
+
+        while (elapsedTime < slideCameraTime)
+        {
+            RaiseCamera(initCameraPos, elapsedTime);
+
+            elapsedTime += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+
+        playerCamera.transform.localPosition = cameraHeight;
+    }
+
+    public IEnumerator SlamMove()
+    {
+        inputManager.SetSlammingOn();
+
+        speedMultiplyer = slamMultiplyer;
+        delayFrames = 0;
+
+        while (inputManager.IsSlamming())
+        {
+            SetVelocity(-playerBody.transform.up * slamSpeed);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public IEnumerator GrappleMove()
+    {
+        float currentLength = 0;
+        Vector3 initPosition;
+
+        while (currentLength < grappleRange && inputManager.IsGrappling())
+        {
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            inputManager.SetOnWall(collision.contacts[0].normal);
+            inputManager.SetSlashingOff();
+            Debug.LogError("On Wall");
+            playerCollider.material = onWall;
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            inputManager.SetOnGround();
+            Debug.LogError("On Ground");
+            playerCollider.material = grounded;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            //SetVelocity(GetVelocity() + collision.contacts[0].normal * Mathf.Clamp(Vector3.Dot(GetVelocity(), -collision.contacts[0].normal), 0f, 100f));
+            if (Vector3.Dot(-collision.contacts[0].normal, worldToLocal * velocityVector) >= 0)
+                speedMultiplyer = 0.1f;
+            else
+                speedMultiplyer = 1.5f;
+
+            delayFrames = 0;
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            inputManager.SetOnGround();
+            playerCollider.material = grounded;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            inputManager.SetOffWall();
+            Debug.LogError("Off Wall");
+        }
+        else if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.LogWarning("Off Ground");
+            inputManager.SetOffGround();
+        }
+    }
+
+    void SetVelocity(Vector3 newVelocity)
+    {
+        playerBody.velocity = newVelocity;
+    }
+
+    Vector3 GetVelocity()
+    {
+        return playerBody.velocity;
+    }
+
+    float GetSpeed()
+    {
+        return playerBody.velocity.magnitude;
+    }
+
+    void LowerCamera(Vector3 initPosition, float elapsedTime)
+    {
+        float clampedT = Mathf.Clamp01(elapsedTime / slideCameraTime);
+
+        float t = Mathf.Lerp(0, 1, clampedT);
+
+        playerCamera.transform.localPosition = Vector3.Lerp(initPosition, cameraHeight + Vector3.down * slideCameraHeight, t);
+    }
+
+    void RaiseCamera(Vector3 initPosition, float elapsedTime)
+    {
+        float clampedT = Mathf.Clamp01(elapsedTime / slideCameraTime);
+
+        float t = Mathf.Lerp(0, 1, clampedT);
+
+        playerCamera.transform.localPosition = Vector3.Lerp(initPosition, cameraHeight, t);
+    }
+
+    public void SetAirResistance(float newResistance)
+    {
+        airResistance = newResistance;
+    }
+}
+
+
+//sasha code
+/*
+
 public class PlayerManager : MonoBehaviour
 {
     //[SerializeField] private InputManager inputManager;
@@ -103,11 +522,21 @@ public class PlayerManager : MonoBehaviour
     void Update()
     {
         //Wall Detection 
-        Vector3 rayDirectionWall = new Vector3 (physicsManager.Velocity.x,0, physicsManager.Velocity.z);
-        if(Physics.Raycast(playerBody.transform.position, rayDirectionWall,out RaycastHit hitinfoWall, .5f, wall))
-        {
-            physicsManager.ApplyForce(-physicsManager.Velocity * mass, 1f);
+        Vector3 horizontalVelocity = new Vector3(physicsManager.Velocity.x, 0, physicsManager.Velocity.z);
+        Vector3 direction = horizontalVelocity.normalized;
 
+        if (horizontalVelocity.magnitude > 0.01f)
+        {
+            float radius = 0.5f;
+            float castDistance = 0.3f;
+
+            if (Physics.SphereCast(playerBody.position, radius, direction, out RaycastHit hitWall, castDistance, wall))
+            {
+                // Stop horizontal movement when colliding with wall
+                Vector3 newVelocity = physicsManager.Velocity;
+                newVelocity = Vector3.ProjectOnPlane(newVelocity, hitWall.normal); // slide along the wall
+                physicsManager.Velocity = new Vector3(newVelocity.x, physicsManager.Velocity.y, newVelocity.z);
+            }
         }
         Vector3 rayDirectionGround = new Vector3(0, -1, 0);
         if (Physics.Raycast(playerBody.transform.position, rayDirectionGround, out RaycastHit hitinfoGround, .5f, ground))
@@ -246,7 +675,7 @@ public class PlayerManager : MonoBehaviour
     }
 
 
-    /*
+    
     public void OnSlash()
     {
 
@@ -255,8 +684,7 @@ public class PlayerManager : MonoBehaviour
         Debug.Log(slashForce);
         physicsManager.ApplyForce(slashForce*slashStrength,1);
     }
-    */
-
+   
 
     public void Look(InputAction.CallbackContext context)
     {
@@ -283,7 +711,7 @@ public class PlayerManager : MonoBehaviour
   //      physicsManager.ZeroYVelocity();
   //  }
   //}
-    /*
+
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
@@ -292,7 +720,7 @@ public class PlayerManager : MonoBehaviour
             physicsManager.ApplyForce(-physicsManager.Velocity * mass, 1f);
         }
     }
-    */
+  
     public void WallJump(Vector3 wallNormal)
       {
           wallNormal = Vector3.ProjectOnPlane(wallNormal, playerBody.transform.up).normalized;
@@ -541,4 +969,4 @@ public class PlayerManager : MonoBehaviour
     //  {
     //      airResistance = newResistance;
     //  }
-}
+} */
